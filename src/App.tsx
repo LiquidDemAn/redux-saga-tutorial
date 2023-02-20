6f6fe1ed0545527b2098e346878133cac975c72b
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2023-02-20 15:24:02 +0200

    App navigation was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { Header } from './components/header';
 import { getNews } from './redux/actions/actions-creator';
 import { useAppDispatch } from './redux/store/hooks';
+import { Route, Routes } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Home } from './pages/home';
+import { PopularNews } from './pages/popular-news';
+import { LatestNews } from './pages/latest-news';
+import { Layout } from './components/layout';
 
 function App() {
+	const history = createBrowserHistory();
 	const dispatch = useAppDispatch();
 
 	const handleNews = () => {
@@ -10,10 +16,13 @@ function App() {
 	};
 
 	return (
-		<>
-			<Header />
-			{/* <main>{children}</main> */}
-		</>
+		<Routes>
+			<Route element={<Layout />}>
+				<Route path='/' element={<Home />} />
+				<Route path='/popular-news' element={<PopularNews />} />
+				<Route path='/latest-news' element={<LatestNews />} />
+			</Route>
+		</Routes>
 	);
 }
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import './header.css';
 
 const navigation = [
 	{ id: 1, title: 'Home', path: '/' },
